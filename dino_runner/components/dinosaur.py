8a446e51e7e4feb8ca124d07d88d96a474c6fafe
0megadestroyer <dystopicdestiny@gmail.com>
2022-12-06 00:12:48 -0400

    Class 1: Dinosaur
    Tarea, hacer que el dinosaurio se agache
    Listo

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,7 +1,7 @@
 import pygame
 
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import JUMPING, RUNNING
+from dino_runner.utils.constants import DUCKING, JUMPING, RUNNING
 
 
 class Dinosaur(Sprite):
@@ -18,24 +18,37 @@ class Dinosaur(Sprite):
         self.step_index = 0
         self.running = True
         self.jumping = False
+        self.ducking = False
 
     def update(self, user_imput):
         if self.running:
             self.run()
         elif self.jumping:
             self.jump()
+        if self.ducking:
+            self.duck()
 
         if user_imput[pygame.K_UP] and not self.jumping:
             self.jumping = True
             self.running = False
+            self.ducking = False
+        elif user_imput[pygame.K_DOWN] and not self.jumping:
+            self.jumping = False
+            self.running = False
+            self.ducking = True
         elif not self.jumping:
             self.running = True
+            self.ducking = False
+            self.jumping = False
 
         if self.step_index >= 10:
             self.step_index = 0
 
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
         self.step_index += 1
 
     def jump(self):
@@ -46,6 +59,13 @@ class Dinosaur(Sprite):
             self.dino_rect.y = self.Y_POS
             self.jumping = False
             self.jump_velocity = self.JUMP_VELOCITY
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS + 40
+        self.step_index += 1
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
